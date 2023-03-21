69bdb1cb73ba75f200268ee0c49c58b878895fc5
Bavaan Jsc <70865397+bavaan-platform@users.noreply.github.com>
2023-03-22 00:46:34 +0700

    Update

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 cd $GITHUB_WORKSPACE
-ls -l
+git config --global --add safe.directory $GITHUB_WORKSPACE
 git rev-parse $INPUT_BRANCH
 caprover deploy -h $INPUT_SERVER -p $INPUT_PASSWORD -b $INPUT_BRANCH -a $INPUT_APPNAME 
